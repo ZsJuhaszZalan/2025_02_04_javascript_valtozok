357f813b344a73ee511360effaeac6cdd511865a
ZsJuhaszZalan <zsiros-juhasz.zalan@diak.szbi-pg.hu>
2025-02-04 10:32:50 +0100

    peldak altozokra

---
+++ 01_valtozok.js
@@ -0,0 +1,22 @@
+// Változó deklarálásának használat előtt megjelenítés: ELAVULT
+var a = 2;
+console.log(a);
+
+// Változó értékének módosítása
+let b = 3;
+b = 4;
+console.log(b);
+const c = 5;
+console.log(c);
+// c = 6; hiba
+
+// Var változó újrafiniálása
+var x = 5;
+console.log(x);
+var x ="alma";
+console.log(x);
+
+let d = 3;
+console.log(d);
+// let d = 4;
+// console.log(d);
---
+++ 02_valtozo_tipusok.js
@@ -0,0 +1,40 @@
+let e = 2;
+console.log('e= ' + e + 'tipusa: ' + typeof(e));
+const pi = 3.14;
+console.log('pi= ' + pi + 'tipusa: ' + typeof(pi));
+
+console.log(0.1 + 0.2);
+console.log((0.1 + 0.2).toFixed(2));
+
+console.log(1/0);
+console.log(-1/0);
+
+// String
+let firstname = 'John';
+let lastname = 'Doe';
+// Template literals (Template strings)
+// backtick (`)
+console.log(`Hello, ${firstname} ${lastname}`);
+
+let person = {
+    name: 'Eminem',
+    age: 39,
+    isStudent: false
+};
+
+console.log(person);
+console.log(person.name);   
+console.log(person.age);
+console.log(person.isStudent);
+console.log(person.address);
+
+let xy;
+console.log(xy);
+
+// Boolean
+console.log('BOOLEAN');
+console.log(typeof true);
+console.log(Boolean(0));
+console.log(Boolean(""));
+console.log(Boolean("a"));
+console.log(Boolean(1));
